50d0cad40507debc9e2694f6edde70af6877b1ec
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-03-14 02:56:12 +0300

    Add a complexity analysis to the space problem

--- src/03 Basic Problems/3.1 Print Elements.js
+++ src/03 Basic Problems/3.1 Print Elements.js
@@ -4,6 +4,10 @@
 
 const SinglyLinkedList = require("../02 Representation/2.1 singly_linked_list");
 
+// The time complexity of the printLinkedList() function is O(n), where n is the length of the linked list. This is because the function iterates through the linked list once, visiting each node exactly once, and performs a constant-time operation for each node (printing its value).
+
+// The space complexity of the function is O(1), as it uses a constant amount of additional space regardless of the size of the input linked list. Specifically, the function only uses a single variable (current) to keep track of the current node being visited in the linked list, and it does not create any new data structures or arrays that grow in size with the input.
+
 function printLinkedList(head) {
   let current = head;
   while (current) {
