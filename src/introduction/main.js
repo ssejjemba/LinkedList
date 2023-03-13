3f4ce58ed9e18ec466c2cc26bf3a96086875ed3e
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-03-14 00:51:45 +0300

    Add definition for linked lists and brief intro

---
+++ src/introduction/main.js
@@ -0,0 +1,14 @@
+/**
+ * Linked lists introduction
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                        What are Linked Linked lists                        */
+/* -------------------------------------------------------------------------- */
+
+// A Linked List is a Linear Data Structure which means that there is a continous contigence relationship between
+// the members of the datastructure. A Linked List doesn't use memory allocation to enforce contigence unlike an array.
+// The members of a linked list can be stored in random spaces in memory but relies on neighbour pointers to enforce contigence
+// of members. The List has two special pointers (Head, Tail), which reference the begining and end of the list respectively.
+// This then means the access is Linear from a Head node to the Tail Node. And the basic type of Linked List is one without any
+// members. We'd represent this by the Head and Tail nodes all pointing at null. In this state the list is considered empty.
